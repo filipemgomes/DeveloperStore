880e78cedd642e216bc5f7de3f73d3f1724bb9de
Filipe <esx78@localiza.com>
2025-02-14 18:37:38 -0300

    feat: add domain events for sales and items

---
+++ src/Ambev.DeveloperEvaluation.Domain/Events/ItemCancelledEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Events
+{
+    public class ItemCancelledEvent
+    {
+        public Guid SaleItemId { get; }
+        public Guid SaleId { get; }
+        public DateTime CancelledAt { get; }
+
+        public ItemCancelledEvent(Guid saleItemId, Guid saleId, DateTime cancelledAt)
+        {
+            SaleItemId = saleItemId;
+            SaleId = saleId;
+            CancelledAt = cancelledAt;
+        }
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Domain/Events/SaleCreatedEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Events
+{
+    public class SaleCreatedEvent
+    {
+        public Guid SaleId { get; }
+        public DateTime SaleDate { get; }
+        public string Customer { get; }
+
+        public SaleCreatedEvent(Guid saleId, DateTime saleDate, string customer)
+        {
+            SaleId = saleId;
+            SaleDate = saleDate;
+            Customer = customer;
+        }
+    }
+}
